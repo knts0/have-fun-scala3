44856e6c898ada16eef95c28dfc75a2c5b50d5e0
Kana Otawara <kana.otawara@nextbeat.net>
2021-10-15 17:21:40 +0900

    solve question 2

--- exercise/src/main/scala/exercise/chapter3/Question2.scala
+++ exercise/src/main/scala/exercise/chapter3/Question2.scala
@@ -2,6 +2,11 @@ package exercise.chapter3
 
 /** Question: Using Ord */
 object Question2:
-
-  def max[T](list: List[T]): T = ???
+  def max[T: Ord](list: List[T])(using ev: Ord[T]): T =
+    if list.isEmpty then throw new IllegalArgumentException()
+    list.reduce { (acc, item) =>
+      val cmp = ev.compare(acc, item)
+      if cmp < 0 then item
+      else acc
+    }
 end Question2
