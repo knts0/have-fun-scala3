2bc060c60a0c8e38f83e85a28d8d1ebd11a1a21d
Kana Otawara <kana.otawara@nextbeat.net>
2021-10-15 18:14:09 +0900

    solve Question3

--- exercise/src/main/scala/exercise/chapter3/Question3.scala
+++ exercise/src/main/scala/exercise/chapter3/Question3.scala
@@ -1,13 +1,23 @@
 package exercise.chapter3
 
+import Question1.given
+
 /** Question: ExtensionMethods */
 object Question3:
 
   extension [T](x: T)
-    def < (y: T): Boolean = ???
-    def > (y: T): Boolean = ???
+    def < (y: T)(using ord: Ord[T]): Boolean = ord.compare(x, y) == -1
+    def > (y: T)(using ord: Ord[T]): Boolean = ord.compare(x, y) == 1
+
+  // これもOK
+//  extension [T](x: T)
+//    def < (y: T)(using Ord[T]): Boolean = summon[Ord[T]].compare(x, y) == -1
+//    def > (y: T)(using Ord[T]): Boolean = summon[Ord[T]].compare(x, y) == 1
 
   extension [T](list: List[T])
-    def max: T = ???
+    // 名前つけなくてもQuestion2のmaxに渡される
+    def max(using Ord[T]): T = {
+      Question2.max(list)
+    }
 
 end Question3
